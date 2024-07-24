7bf817acf54f50903196b3f322142b8cb046d89b
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2024-07-24 16:24:11 +0200

    Code maintenance

--- src/test/java/io/github/poshjosh/ratelimiter/annotations/Rhs.java
+++ src/test/java/io/github/poshjosh/ratelimiter/annotations/Rhs.java
@@ -2,12 +2,14 @@ package io.github.poshjosh.ratelimiter.annotations;
 
 public interface Rhs<V> {
 
-    @LongCondition(that="system.time.elapsed", is=">", rhs=1)
+    @LongCondition(that="sys.time.elapsed", is=">", rhs=1)
     V getKey();
 
-    @LongCondition(lhs="system.time.elapsed", op=">", rhs=1)
+    @LongCondition(lhs="sys.time.elapsed", op=">", rhs=1)
     V getValue();
 
     @SysTimeElapsed(is=">", rhs=1)
+    //@SysTimeElapsed(">1")
+    //@RequestUserRole("=ROLE_GUEST")
     V getSomething();
 }
