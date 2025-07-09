36cf9b6d452a61655922ec04179529911e3ec12c
Chinomso Ikwuagwu <chinomso.ikwuagwu@unite.eu>
2025-07-09 16:21:20 +0200

    Use fallback instead of Optional, use a cache too

--- src/main/java/io/github/poshjosh/ratelimiter/util/AbstractMatcherProvider.java
+++ src/main/java/io/github/poshjosh/ratelimiter/util/AbstractMatcherProvider.java
@@ -5,19 +5,29 @@ import io.github.poshjosh.ratelimiter.matcher.Matcher;
 import io.github.poshjosh.ratelimiter.matcher.Matchers;
 import io.github.poshjosh.ratelimiter.model.Rate;
 import io.github.poshjosh.ratelimiter.model.RateConfig;
+import io.github.poshjosh.ratelimiter.store.Store;
+import io.github.poshjosh.ratelimiter.store.Stores;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public abstract class AbstractMatcherProvider<INPUT> implements MatcherProvider<INPUT> {
 
     private final ExpressionMatcher<INPUT> expressionMatcher;
 
+    private final Store<String, Matcher<INPUT>> matchersStore;
+
     protected AbstractMatcherProvider(ExpressionMatcher<INPUT> expressionMatcher) {
         this.expressionMatcher = Objects.requireNonNull(expressionMatcher);
+
+        // We decided no need for thread safety for the cache. This is because
+        // it is ok to override an existing value with a new value as we only
+        // use this cache to prevent creating the same matcher multiple times.
+
+        // TOD0: Try this
+        // TODO: Make the capacity of the matchers cache configurable
+//        this.matchersStore = Stores.ofLRU(10_000, 0.75f);
+        this.matchersStore = Stores.noop();
     }
 
     @Override
@@ -27,21 +37,32 @@ public abstract class AbstractMatcherProvider<INPUT> implements MatcherProvider<
             return Collections.emptyList();
         }
         if (subRates.size() == 1) {
-            return createExpressionMatcher(subRates.get(0).getCondition())
-                    .map(Collections::singletonList)
-                    // Tag:Rule:number-of-matchers-must-equal-number-of-rates
-                    .orElse(Collections.singletonList(Matchers.matchNone()));
+            Matcher<INPUT> matcher =
+                    expressionMatcherOrFallback(
+                            subRates.get(0).getCondition(), Matchers.matchNone());
+            // Tag:Rule:number-of-matchersStore-must-equal-number-of-rates
+            return Collections.singletonList(matcher);
         }
         return subRates.stream()
-                .map(rate -> createExpressionMatcher(rate.getCondition()).orElse(Matchers.matchNone()))
+                .map(rate -> expressionMatcherOrFallback(rate.getCondition(), Matchers.matchNone()))
                 .collect(Collectors.toList());
     }
 
-    protected Optional<Matcher<INPUT>> createExpressionMatcher(String expression) {
+    //@Nullable
+    protected Matcher<INPUT> expressionMatcherOrFallback(
+            String expression, Matcher<INPUT> fallback) {
         if (expression == null || expression.isEmpty()) {
-            return Optional.empty();
+            return fallback;
         }
-        return expressionMatcher.matcher(expression);
+
+        Matcher<INPUT> result = matchersStore.get(expression);
+        if (result != null) {
+            return result;
+        }
+
+        result = expressionMatcher.matcherOrFallback(expression, fallback);
+        matchersStore.put(expression, result);
+        return result;
     }
 
     protected boolean isMatchNone(RateConfig rateConfig, boolean isExpressionPresent) {
